c89a2150d31994a61c650bc5abfff031faecddf1
Chik3r <ikervaraalarcon@gmail.com>
2021-04-23 10:01:55 -0400

    Initial Commit

---
+++ TMLPatcher_UI/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace TMLPatcher_UI.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
