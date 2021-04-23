652c783c2efd20749df568451099c9edce53cd2a
Chik3r <ikervaraalarcon@gmail.com>
2021-04-23 13:56:18 -0400

    Make MainWindow populated

--- TMLPatcher_UI/ViewModels/MainWindowViewModel.cs
+++ TMLPatcher_UI/ViewModels/MainWindowViewModel.cs
@@ -1,11 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using TMLPatcher_UI.Models;
 
 namespace TMLPatcher_UI.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting => "Welcome to Avalonia!";
+        public MainWindowViewModel()
+        {
+            // TODO: Populate the file listing automatically by getting all files inside the mods folder
+            // TODO: Pop-up that asks the user for the path to the mods folder
+            // TODO: Store path to mods folder in configuration file
+            List<FileItem> files = new()
+            {
+                new FileItem("CalamityMod", "C:\\Users\\ikerv\\Documents\\My Games\\Terraria\\ModLoader\\Mods\\CalamityMod.tmod"),
+                new FileItem("CheatSheet", "C:\\Users\\ikerv\\Documents\\My Games\\Terraria\\ModLoader\\Mods\\CheatSheet.tmod")
+            };
+
+            // TODO: Remove debug items
+            for (int i = 0; i < 50; i++)
+                files.Add(new FileItem($"Item {i}", "C:\\Users\\ikerv\\Documents\\My Games\\Terraria\\ModLoader\\Mods\\CheatSheet.tmod"));
+
+            FileListing = new FileListingViewModel(files);
+        }
+        
+        public FileListingViewModel FileListing { get; }
     }
 }
