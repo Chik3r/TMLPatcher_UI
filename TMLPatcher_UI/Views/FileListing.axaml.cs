19671d3705890d25b41c723f5a42dee435b23a62
Chik3r <ikervaraalarcon@gmail.com>
2021-04-23 11:04:38 -0400

    Add a simple listbox

---
+++ TMLPatcher_UI/Views/FileListing.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TMLPatcher_UI.Views
+{
+    public class FileListing : UserControl
+    {
+        public FileListing()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
