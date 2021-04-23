6c7b7e4f44de282306e2a54baa643fd99c4a4f4c
Chik3r <ikervaraalarcon@gmail.com>
2021-04-23 18:46:53 -0400

    Forgot a file

---
+++ TMLPatcher_UI/Views/FileManageView.axaml.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace TMLPatcher_UI.Views
+{
+    public class FileManageView : UserControl
+    {
+        public FileManageView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
