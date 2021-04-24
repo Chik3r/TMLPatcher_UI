c28e31149275bca3a6d1397a23d090a3c772aa79
Chik3r <ikervaraalarcon@gmail.com>
2021-04-23 20:43:51 -0400

    Add extract dir

--- TMLPatcher_UI/Program.cs
+++ TMLPatcher_UI/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.IO;
 using Avalonia;
-using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.ReactiveUI;
 
 namespace TMLPatcher_UI
 {
     class Program
     {
+        public static string ExtractDirectory => Path.Combine(Environment.CurrentDirectory, "Extracted");
+        
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
