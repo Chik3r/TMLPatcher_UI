ba5f49fbde5558bc17a520d3375307bffcf60d2e
Chik3r <ikervaraalarcon@gmail.com>
2021-07-16 23:26:41 -0400

    finish repack

--- TMLPatcher_UI/ViewModels/RepackViewModel.cs
+++ TMLPatcher_UI/ViewModels/RepackViewModel.cs
@@ -1,112 +1,101 @@
-﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.IO;
-using System.Threading.Tasks;
-using Avalonia.Controls;
+using System.Text.RegularExpressions;
 using ReactiveUI;
+using TML.Files.ModLoader.Files;
 using TML.Patcher.Packing;
 using TMLPatcher_UI.Models;
-using TMLPatcher_UI.Views;
 
 namespace TMLPatcher_UI.ViewModels
 {
     public class RepackViewModel : ViewModelBase
     {
-        private bool _currentlyExtracting;
-        private int _extractProgress;
         private FileItem _selectedFile;
-        private ProgressReporter _progressReporter;
+        private string _modName;
+        private string _modVersion;
+        private string _tmlVersion;
+        private bool _canRepack;
 
         public RepackViewModel()
         {
             Files = new ObservableCollection<FileItem>();
-            CurrentlyExtracting = false;
         }
 
         public ObservableCollection<FileItem> Files { get; }
 
-        public bool CurrentlyExtracting
-        {
-            get => _currentlyExtracting;
-            set => this.RaiseAndSetIfChanged(ref _currentlyExtracting, value);
-        }
-
-        public int ExtractProgress
-        {
-            get => _extractProgress;
-            set => this.RaiseAndSetIfChanged(ref _extractProgress, value);
-        }
-
         public FileItem SelectedFile
         {
             get => _selectedFile;
-            set => this.RaiseAndSetIfChanged(ref _selectedFile, value);
-        }
-
-        public async void Populate()
-        {
-            // Create a new folder dialog
-            OpenFolderDialog folderDialog = new();
-            folderDialog.Title = "Select your Mods folder";
-
-            // Wait for the user to select the folder
-            string folder = await folderDialog.ShowAsync(MainWindow.Instance);
-            
-            // Check if the folder exists
-            if (string.IsNullOrEmpty(folder))
-                return;
-
-            if (!Directory.Exists(folder))
-                return;
-
-            // Get all files in the folder and add them to the listbox
-            string[] files = Directory.GetFiles(folder, "*.tmod");
-            foreach (string file in files) Files.Add(new FileItem(Path.GetFileNameWithoutExtension(file), file));
-        }
-
-        public void ExtractMod()
-        {
-            // Start the unpack request with a new progress reporter
-            // The progress reporter will update the progress bar
-            _progressReporter = new ProgressReporter();
-            UnpackRequest request = new UnpackRequest(
-                Directory.CreateDirectory(Path.Combine(Program.ExtractDirectory, SelectedFile.FileName)),
-                SelectedFile.FilePath, 4, _progressReporter);
-
-            _progressReporter.UpdateProgress += UpdateProgress;
-
-            // Start a new task so that the unpack request runs in a separate thread from the ui
-            Task.Run(() => request.ExecuteRequest());
-            _progressReporter.Start();
-
-            // Disable the extract mod button
-            CurrentlyExtracting = true;
-        }
-
-        private void UpdateProgress(int i)
-        {
-            // Set the extract progress to the progress
-            ExtractProgress = i;
-
-            // Check if the extraction has completed
-            if (i == 100)
+            set
             {
-                _progressReporter.Finish();
-                CurrentlyExtracting = false;
-
-                // Format the path to the extracted folder
-                string folderPath = Path.Combine(Program.ExtractDirectory, SelectedFile.FileName);
-                if (!folderPath.EndsWith(Path.DirectorySeparatorChar))
-                    folderPath += Path.DirectorySeparatorChar;
-                
-                // Open the folder
-                Process.Start(new ProcessStartInfo
-                {
-                    FileName = folderPath,
-                    UseShellExecute = true,
-                    Verb = "open"
-                });
+                this.RaiseAndSetIfChanged(ref _selectedFile, value);
+                ModName = value.FileName;
             }
+        }
+
+        public string ModName
+        {
+            get => _modName;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _modName, value);
+                UpdateCanRepack();
+            }
+        }
+
+        public string ModVersion
+        {
+            get => _modVersion;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _modVersion, value);
+                UpdateCanRepack();
+            }
+        }
+
+        public string TmlVersion
+        {
+            get => _tmlVersion;
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _tmlVersion, value);
+                UpdateCanRepack();
+            }
+        }
+
+        public bool CanRepack
+        {
+            get => _canRepack;
+            set => this.RaiseAndSetIfChanged(ref _canRepack, value);
+        }
+
+        public void UpdateCanRepack() =>
+            CanRepack = !(string.IsNullOrWhiteSpace(ModName) || !Version.TryParse(ModVersion, out _) ||
+                          !Version.TryParse(TmlVersion, out _));
+
+        public void Populate()
+        {
+            DirectoryInfo extractDir = Directory.CreateDirectory(Program.ExtractDirectory);
+
+            // Get all directories in extract dir
+            foreach (DirectoryInfo dir in extractDir.EnumerateDirectories())
+                Files.Add(new FileItem(dir.Name, dir.FullName));
+        }
+
+        public void RepackMod()
+        {
+            DirectoryInfo repackDir = new(SelectedFile.FilePath);
+            string targetFilePath = Path.ChangeExtension(Path.Combine(Program.RepackDirectory, SelectedFile.FileName), ".tmod");
+
+            Directory.CreateDirectory(Program.RepackDirectory);
+
+            ModData data = new(ModName, Version.Parse(ModVersion), Version.Parse(TmlVersion));
+
+            // TODO: convert threads to option
+            RepackRequest request = new(repackDir, targetFilePath, data, 4);
+
+            request.ExecuteRequest();
         }
     }
 }
